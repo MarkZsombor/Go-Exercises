4288a6fced4b0bad3d8190a79d9b6970491d5697
Mark Zsombor <markszsombor@gmail.com>
2018-03-12 14:24:14 -0700

    added comments

--- simple-server/main.go
+++ simple-server/main.go
@@ -1,14 +1,16 @@
 package main
 
+// Runs an HTTP server that will show the phrase "Hello <name>" when you go to localhost:8000/<name>
 import (
 	"net/http"
 	"strings"
 )
 
+// Get the URL param then forms a string to be displayed on page
 func sayHello(w http.ResponseWriter, r *http.Request) {
 	message := r.URL.Path
 	message = strings.TrimPrefix(message, "/")
-	message = "Hello " + message
+	message = "Hello " + message + ". Thanks for trying my program!"
 
 	w.Write([]byte(message))
 }
