316497f11cc0c46060c37519254c7c0c92ec7131
Mark Zsombor <markszsombor@gmail.com>
2018-03-12 20:55:40 -0700

    refactored code for fizzbuzz

--- fizzbuzz/main.go
+++ fizzbuzz/main.go
@@ -6,9 +6,8 @@ import (
 
 /* The classic, cycle through the numbers from 1-100, if a number is divisible by 3 print Fizz, if its divisible by 5 print Buzz, divisible by 15 print FizzBuzz, else print the number */
 func fizzBuzz() {
-	var output string
 	for i := 1; i <= 100; i++ {
-		output = ""
+		output := ""
 		if i%3 == 0 {
 			output += "Fizz"
 		}
