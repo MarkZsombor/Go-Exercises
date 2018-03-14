da2390843eea06e598d396bfb7a63b261fa1b6ee
Mark Zsombor <markszsombor@gmail.com>
2018-03-14 12:03:10 -0700

    made a second variant of the fibonacci, all funcs now return values instead of printing

--- fibonacci/main.go
+++ fibonacci/main.go
@@ -4,18 +4,37 @@ import (
 	"fmt"
 )
 
-func fibonacci(n int) {
-	var fibSeq []int
+// Stores all values in an array which is used to calc next value
+func fibonacciArray(n int) (fibSeq []int) {
 	fibSeq = append(fibSeq, 0)
 	fibSeq = append(fibSeq, 1)
 	for i := 1; i < n; i++ {
 		fibSeq = append(fibSeq, (fibSeq[i] + fibSeq[i-1]))
 	}
-	fmt.Println(fibSeq)
+	return
+}
+
+// Stores only variables needed for next calculation
+func fibonacci(n int) (sum int) {
+	a := 0
+	b := 1
+	for i := 1; i < n; i++ {
+		sum = a + b
+		a = b
+		b = sum
+	}
+	return
 }
 
 func main() {
-	fibonacci(4)
-	fibonacci(10)
-	fibonacci(20)
+
+	fmt.Println(fibonacci(4))
+	fmt.Println(fibonacciArray(4))
+
+	fmt.Println(fibonacci(10))
+	fmt.Println(fibonacciArray(10))
+
+	fmt.Println(fibonacci(20))
+	fmt.Println(fibonacciArray(20))
+
 }
