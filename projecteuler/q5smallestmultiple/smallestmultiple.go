34b6723a0d9b6b1ec7237f05c52073aca9784c7f
Mark Zsombor <markszsombor@gmail.com>
2018-03-23 13:36:23 -0700

    tried other method, added comments for q5

--- projecteuler/q5smallestmultiple/smallestmultiple.go
+++ projecteuler/q5smallestmultiple/smallestmultiple.go
@@ -1,13 +1,18 @@
 package main
 
+import (
+	"fmt"
+)
+
 //what is the smallest positive number that is evenly divisible by all numbers 1-20
 
-// Doesnt work for some reason
+// Values given, when multiplied out, is too big by a factor of 3 with j*j!=i, too small by almost factor of 10 without
+// Probably the wrong approach in general
 func uniqueFactors(N int) (factors []int) {
 	for i := N; i > 0; i-- {
 		hasFactors := false
 		for j := 2; j < i; j++ {
-			if i%j == 0 && j*j != i {
+			if i%j == 0 { //&& j*j != i {
 				hasFactors = true
 			}
 		}
@@ -26,6 +31,8 @@ func multiplyArray(arr []int) (product int) {
 	return
 }
 
+// This works, but WOW its brute forcing the issue and takes many seconds to complete
+// Look into finding a better algorithm
 func findDivisibleNumber(N int) int {
 	testValue := N + 1
 	for {
@@ -44,4 +51,7 @@ func findDivisibleNumber(N int) int {
 
 func main() {
 	// fmt.Println(findDivisibleNumber(20)) //232792560
+	uf := uniqueFactors(10)
+	fmt.Print(uf)
+	fmt.Println(multiplyArray(uf))
 }
